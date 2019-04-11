4c9975f79c8f0a722fd76b7b0e84d5851108042c
rhys graham <c3302909@uon.edu.au>
2019-04-11 11:49:55 +0000

    template

--- main.c
+++ main.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include <math.h>
+
+// prototype
+char rotEncryption(char c)
+char subEncryption(char c)
+char rotDecryption(char c)
+char subDecryption(char c)
+int main(){
+    //menu switch case
+    rotEncryption();
+    subEncryption();
+    rotDecryption();
+    subDecryption();
+}
+
+//def
