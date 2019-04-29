e9736b91f4792cdd8f6a12105ab97891b9d9635a
rhys graham <c3302909@uon.edu.au>
2019-04-29 01:54:30 +0000

    encryption and decryption with rotation work, menu works, cannot process spaces between words, substitution to hard

--- main.c
+++ main.c
@@ -4,61 +4,79 @@
 
 // prototype
 void rotEncryption(void);
-//void rotDecryption(void);
+void rotDecryption(void);
+//could not figure out substitution
 //void subEncryption(void);
 //void subDecryption(void);
 
 int main(){
-
    //Printf menu
-   printf("Please choose a number: \n");
+   printf("Options\n");
    printf("1. Rotation Cipher Encryption\n");
    printf("2. Rotation Cipher Decryption\n");
-   printf("3. Substitution Cipher Encryption\n");
-   int c;
-   scanf("%d", &c);
-   // determines which one to use
-    switch (c) {
-        case 1:
-            rotEncryption();
-        break;
-        
-        /*
-        case 2:
-            printf("Please enter rotation amount between 0 & 25:\n");
-            scanf("%d", &k);
-            rotDecryption(message, text, k);
-        break; 
-
-        case 3:
-            subEncryption(message, text);
+   printf("Please choose a number: \n");
+   // determines which method
+   int c=0;
+   do{
+        //reads user selection, and chooses which function to call
+        scanf("%d", &c); 
+        switch(c){
+            case 1:
+                rotEncryption();  
             break;
-        case 4:
-            subDecryption(message, text);
+            case 2:
+                rotDecryption();   
             break;
-      */
- 
-        default:
-            printf("Please enter valid input (1, 2, 3, 4).\n");
-            break;
-    }
+            default:
+                printf("Please enter valid input\n");
+            
+        }
+    }while(c>2);
 }
-
 //def
 // encrypts the rotation cipher
 void rotEncryption(void){
-   char text[] = "This is a test";
-   int k = 5;
-   for(int i=0; i < 18; i++){
+   char text[1024];
+   printf("Please enter text to encrypt followed by a key\n");
+   //scanf reads spaces with %[0-9a-zA-Z]
+   scanf(" %[0-9a-zA-Z] ", text); 
+   int k;
+   scanf("%d", &k);
+   for(int i=0; i < 12; i++){
        if(text[i] != 32 && text[i] !=0){
            //convert ascii
            text[i] = text[i] - 97; 
            // letter shift based on key
            text[i] = text[i] + k;
+           //overflow
            text[i] = text[i] % 26;
            //converts back to alphabet
            text[i] = text[i] + 97;
-       }
+       } 
    }
     printf("%s\n", text);
 }
+//decrypts given message
+void rotDecryption(void){
+   char text[1024];
+   printf("Please enter text to decrypt followed by a key\n");
+   //scanf reads spaces with %[0-9a-zA-Z]
+   scanf(" %[0-9a-zA-Z] ", text);
+   int k;
+   scanf("%d", &k);
+   //decryption process
+   for (int i=0; i<400; i++){
+        if(text[i] != 32 && text[i] != 0){
+            //convert ascii
+            text[i] = text[i] - 97; 
+            // complete shift
+            text[i] = text[i] + (26 - k);
+            //overflow
+            text[i] = text[i] % 26;
+            //converts back to alphabet
+            text[i] = text[i] + 97;
+        }
+    }
+    printf("%s\n", text);
+}
+
